a034c9a761b47a2729a6a185e11aa067ae51f7e6
ADI-1900 <djun_igari@hotmail.com>
2016-05-28 14:16:52 -0300

    Cap 1.4 Hello World 2

--- Android/HelloAndroid/app/src/main/java/br/com/djun/helloandroid/MainActivity.java
+++ Android/HelloAndroid/app/src/main/java/br/com/djun/helloandroid/MainActivity.java
@@ -2,15 +2,29 @@ package br.com.djun.helloandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity{
 
+    private EditText nomeEditText;
+    private TextView saudacaoTextView;
+    private String saudacao;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        nomeEditText = (EditText) findViewById(R.id.nomeEditText);
+        saudacaoTextView = (TextView) findViewById(R.id.saudacaoTextView);
+        saudacao = getResources().getString(R.string.saudacao);
     }
 
+
+    public void surpreenderUsuario(View view){
+        saudacaoTextView.setText(saudacao.concat(" ").concat(nomeEditText.getText().toString()));
+    }
 }
