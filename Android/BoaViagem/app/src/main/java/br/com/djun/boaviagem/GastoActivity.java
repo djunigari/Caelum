baac46cc5fd905453ec293d711f2f8e5f2b2b117
Alexandre <cin_aigari@uolinc.com>
2016-05-30 18:00:00 -0300

    Cap 3.5

--- Android/BoaViagem/app/src/main/java/br/com/djun/boaviagem/GastoActivity.java
+++ Android/BoaViagem/app/src/main/java/br/com/djun/boaviagem/GastoActivity.java
@@ -5,14 +5,17 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 
 import java.util.Calendar;
 
 public class GastoActivity extends Activity {
     int ano,mes,dia;
     private Button dataGastoButton;
+    private Spinner categoriaSpinner;
     private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
@@ -28,11 +31,16 @@ public class GastoActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gasto);
         dataGastoButton = (Button) findViewById(R.id.dataGastoButton);
+        categoriaSpinner = (Spinner) findViewById(R.id.categoriaSpinner);
+
         Calendar calendar = Calendar.getInstance();
         ano = calendar.get(Calendar.YEAR);
         mes = calendar.get(Calendar.MONTH);
         dia = calendar.get(Calendar.DAY_OF_MONTH);
         dataGastoButton.setText(dia + "/" + (mes+1) + "/" + ano);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categoria_gasto, android.R.layout.simple_spinner_item);
+        categoriaSpinner.setAdapter(adapter);
     }
 
     public void selecionarData(View view){
