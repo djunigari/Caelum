28943e78035a9c4450342b39102cfb595a4f9617
Alexandre <cin_aigari@uolinc.com>
2016-06-01 19:14:04 -0300

    Cap 3.9

--- Android/BoaViagem/app/src/main/java/br/com/djun/boaviagem/BoaViagemActivity.java
+++ Android/BoaViagem/app/src/main/java/br/com/djun/boaviagem/BoaViagemActivity.java
@@ -2,14 +2,18 @@ package br.com.djun.boaviagem;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class BoaViagemActivity extends Activity {
+    private static final String MANTER_CONECTADO = "manter_conectado";
     private EditText usuarioEditText;
     private EditText senhaEditText;
+    private CheckBox manterConectado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +21,32 @@ public class BoaViagemActivity extends Activity {
         setContentView(R.layout.login);
         usuarioEditText = (EditText)findViewById(R.id.usuarioEditText);
         senhaEditText = (EditText)findViewById(R.id.senhaEditText);
+        manterConectado = (CheckBox) findViewById(R.id.manterConectado);
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        boolean conectado = preferences.getBoolean(MANTER_CONECTADO, false);
+        if(conectado){
+            startActivity(new Intent(this,DashboardActivity.class));
+        }
+
     }
 
     public void entrarOnClick(View view){
         String usuario = usuarioEditText.getText().toString();
         String senha = senhaEditText.getText().toString();
 
+
         if("leitor".equalsIgnoreCase(usuario) && "123".equals(senha)){
+            SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean(MANTER_CONECTADO,manterConectado.isChecked());
+            editor.commit();
             startActivity(new Intent(this,DashboardActivity.class));
         }else{
             String msg = getString(R.string.erro_autenticao);
             Toast toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
             toast.show();
         }
+
 
     }
 }
---
+++ Android/BoaViagem/app/src/main/java/br/com/djun/boaviagem/ConfiguracoesActivity.java
@@ -0,0 +1,12 @@
+package br.com.djun.boaviagem;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class ConfiguracoesActivity extends PreferenceActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferencias);
+    }
+}
