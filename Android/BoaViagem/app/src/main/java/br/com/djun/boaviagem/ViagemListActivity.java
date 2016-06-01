c2ebcb3b7c5aa51aaaababaf224b660d1cc6aded
ADI-1900 <djun_igari@hotmail.com>
2016-06-01 00:54:32 -0300

    Cap 3.8

--- Android/BoaViagem/app/src/main/java/br/com/djun/boaviagem/ViagemListActivity.java
+++ Android/BoaViagem/app/src/main/java/br/com/djun/boaviagem/ViagemListActivity.java
@@ -1,20 +1,24 @@
 package br.com.djun.boaviagem;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.SimpleAdapter;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ViagemListActivity extends ListActivity implements AdapterView.OnItemClickListener {
+public class ViagemListActivity extends ListActivity implements AdapterView.OnItemClickListener,DialogInterface.OnClickListener {
     private	List<Map<String,Object>> viagens = new ArrayList<>();
+    private int posicaoViagem;
+    private AlertDialog alertDialog;
+    private AlertDialog dialogConfirmacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +31,14 @@ public class ViagemListActivity extends ListActivity implements AdapterView.OnIt
                 R.id.dataTextView,
                 R.id.valorTextView};
         setListAdapter(new SimpleAdapter(this, listarViagens(),R.layout.lista_viagem, de, para));
+        alertDialog = createAlertDialog();
+        dialogConfirmacao = createAlertDialogConfirmacao();
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Map<String, Object> map = viagens.get(position);
-        String destino = (String) map.get("destino");
-        String msg = "Viagem selecionada: "+destino;
-        Toast.makeText(this,msg, Toast.LENGTH_SHORT).show();
-        startActivity(new Intent(this,GastoListActivity.class));
+        posicaoViagem = position;
+        alertDialog.show();
     }
 
     private List<Map<String,Object>> listarViagens() {
@@ -54,4 +57,51 @@ public class ViagemListActivity extends ListActivity implements AdapterView.OnIt
         return viagens;
     }
 
+
+    private AlertDialog createAlertDialog(){
+        final CharSequence[] items= {
+                getString(R.string.editar),
+                getString(R.string.novo_gasto),
+                getString(R.string.gastos_realizados),
+                getString(R.string.remover)
+        };
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.opcoes);
+        builder.setItems(items,this);
+        return builder.create();
+    }
+
+    private AlertDialog createAlertDialogConfirmacao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.confirmacao_exclusao_viagem);
+        builder.setPositiveButton(getString(R.string.sim),this);
+        builder.setNegativeButton(getString(R.string.nao),this);
+        return builder.create();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int item) {
+        switch (item){
+            case 0:
+                startActivity(new Intent(this,ViagemActivity.class));
+                break;
+            case 1:
+                startActivity(new Intent(this,GastoActivity.class));
+                break;
+            case 2:
+                startActivity(new Intent(this,GastoListActivity.class));
+                break;
+            case 3:
+                dialogConfirmacao.show();
+                break;
+            case AlertDialog.BUTTON_POSITIVE:
+                viagens.remove(posicaoViagem);
+                getListView().invalidateViews();
+                break;
+            case AlertDialog.BUTTON_NEGATIVE:
+                dialogConfirmacao.dismiss();
+                break;
+
+        }
+    }
 }
