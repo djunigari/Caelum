66062e70f728a0a9ace741be5f6e6307a4fbbe17
ADI-1900 <djun_igari@hotmail.com>
2016-06-02 00:33:52 -0300

    Cap 4.1

---
+++ Android/BoaViagem/app/src/main/java/br/com/djun/boaviagem/DatabaseHelper.java
@@ -0,0 +1,42 @@
+package br.com.djun.boaviagem;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String BANCO_DADOS = "BoaViagem";
+    private static int VERSAO = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, BANCO_DADOS, null, VERSAO);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE VIAGEM(" +
+                "_id                 INTEGER PRIMARY KEY," +
+                "destino            TEXT," +
+                "tipo_viagem        INTEGER," +
+                "data_chegada       DATE," +
+                "data_saida         DATE," +
+                "orcamento          DOUBLE," +
+                "quantidade_pessoas INTEGER" +
+                ");");
+        db.execSQL("CREATE TABLE GASTO(" +
+                "_id         INTEGER PRIMARY KEY," +
+                "categoria  TEXT," +
+                "data       Date," +
+                "valor      DOUBLE," +
+                "descricao  TEXT," +
+                "local      TEXT," +
+                "viagem_id  INTEGER," +
+                "FOREIGN KEY (viagem_id) REFERENCES viagem(id)" +
+                ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
