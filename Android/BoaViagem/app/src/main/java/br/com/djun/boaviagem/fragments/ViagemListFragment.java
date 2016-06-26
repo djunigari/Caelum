2a934828428f5da46ed4fae9b46ac18bafe88848
ADI-1900 <djun_igari@hotmail.com>
2016-06-25 22:08:07 -0300

    Cap 9.6

--- Android/BoaViagem/app/src/main/java/br/com/djun/boaviagem/fragments/ViagemListFragment.java
+++ Android/BoaViagem/app/src/main/java/br/com/djun/boaviagem/fragments/ViagemListFragment.java
@@ -1,36 +1,42 @@
 package br.com.djun.boaviagem.fragments;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-
-import java.util.Arrays;
-import java.util.List;
 
 import br.com.djun.boaviagem.Constantes;
 import br.com.djun.boaviagem.listeners.AnotacaoListener;
+import br.com.djun.boaviagem.provider.BoaViagemContract;
 
-public class ViagemListFragment extends ListFragment implements OnItemClickListener{
+public class ViagemListFragment extends ListFragment implements OnItemClickListener, LoaderManager.LoaderCallbacks<Cursor>{
     private AnotacaoListener callback;
+    private SimpleCursorAdapter adapter;
 
     @Override
     public void onStart() {
         super.onStart();
-        List<String> viagens = Arrays.asList("Campo Grande", "Sao Paulo", "Miami");
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, viagens);
+        adapter = new SimpleCursorAdapter(getActivity(), android.R.layout.simple_expandable_list_item_1, null,
+                new String[]{BoaViagemContract.Viagem.DESTINO},
+                new int[]{android.R.id.text1}, 0);
         setListAdapter(adapter);
         getListView().setOnItemClickListener(this);
     }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        String viagem  = (String) getListAdapter().getItem(i);
+        long viagem = getListAdapter().getItemId(i);
         Bundle bundle = new Bundle();
-        bundle.putString(Constantes.VIAGEM_SELECIONADA, viagem);
+        bundle.putLong(Constantes.VIAGEM_SELECIONADA,viagem);
         callback.viagemSelecionada(bundle);
     }
 
@@ -38,5 +44,29 @@ public class ViagemListFragment extends ListFragment implements OnItemClickListe
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         callback = (AnotacaoListener) activity;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getLoaderManager().initLoader(0,null,this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Uri uri = BoaViagemContract.Viagem.CONTENT_URI;
+        String[] projection = {BoaViagemContract.Viagem._ID, BoaViagemContract.Viagem.DESTINO};
+
+        return new CursorLoader(getActivity(),uri,projection,null,null,null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        adapter.swapCursor(null);
     }
 }
